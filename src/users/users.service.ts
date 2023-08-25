f440bec1daed98654db7febe6335e22e18de6004
winterrdog <winterrdog@protonmail.ch>
2023-08-25 21:46:29 +0300

    added user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,10 @@
 import { Model } from "mongoose";
-import { Injectable } from "@nestjs/common";
+import {
+    ForbiddenException,
+    Injectable,
+    InternalServerErrorException,
+    NotFoundException,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./schemas/users.schema";
 
@@ -7,5 +12,59 @@ import { User } from "./schemas/users.schema";
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-    // define all methods to retrive data from db
+    // create a new user
+    async createUser(user: User): Promise<User> {
+        try {
+            const newUser = new this.userModel(user);
+
+            return await newUser.save();
+        } catch (_) {
+            throw new InternalServerErrorException(
+                "Internal Server Error occurred",
+            );
+        }
+    }
+
+    // find a single user by id
+    async queryUserById(userId: string): Promise<User> {
+        try {
+            const user = await this.userModel.findById(userId);
+            if (!user) {
+                throw new NotFoundException("User not found.");
+            }
+
+            return user;
+        } catch (_) {
+            throw new InternalServerErrorException(
+                "Internal Server Error occurred",
+            );
+        }
+    }
+
+    // update a driver's status
+    async updateDriverStatus(
+        userId: string,
+        driverStatus: string,
+    ): Promise<User> {
+        try {
+            // find user and check if it's a driver
+            const user = await this.userModel.findById(userId);
+            if (!user) {
+                throw new NotFoundException("User not found.");
+            }
+
+            if (user.role !== "driver") {
+                throw new ForbiddenException("Only drivers can accept rides.");
+            }
+
+            // update driver's status
+            user.driverStatus = driverStatus;
+
+            return await user.save();
+        } catch (_) {
+            throw new InternalServerErrorException(
+                "Internal Server Error occurred",
+            );
+        }
+    }
 }
