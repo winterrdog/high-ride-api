5384d2d1d312bbc9e34002aeb57e469bff6e9740
winterrdog <winterrdog@protonmail.ch>
2023-08-28 16:45:10 +0300

    added validation: ride management

--- src/rides/rides.interface.ts
+++ src/rides/rides.interface.ts
@@ -1,9 +1,22 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 /**
  * class to represent a location
  */
 export class Location {
+    @IsNotEmpty()
+    @IsNumber()
     public readonly latitude: number;
+
+    @IsNotEmpty()
+    @IsNumber()
     public readonly longitude: number;
+
+    @IsNotEmpty()
+    @IsString()
     public readonly locationName: string;
+
+    @IsNotEmpty()
+    @IsString()
     public readonly country: string;
 }
