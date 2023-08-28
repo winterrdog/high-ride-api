c2426cc718e5c1f0c424010c9f1fef28bdc62295
winterrdog <winterrdog@protonmail.ch>
2023-08-28 16:44:08 +0300

    added validation: user management request

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,12 +1,49 @@
+import {
+    IsAlpha,
+    IsEmail,
+    IsEnum,
+    IsNotEmpty,
+    IsPhoneNumber,
+    IsString,
+    MaxLength,
+    MinLength,
+} from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    @IsAlpha()
     readonly firstName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsAlpha()
     readonly lastName: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     readonly email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(64)
     readonly password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsPhoneNumber("UG")
     readonly phoneNumber: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsEnum(["driver", "passenger"])
     readonly role: string;
 }
 
 export class UpdateDriverStatusDto {
+    @IsNotEmpty()
+    @IsString()
+    @IsEnum(["available", "unavailable"])
     readonly driverStatus: "available" | "unavailable";
 }
