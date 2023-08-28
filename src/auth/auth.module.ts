af0eb174673f33f4d70c180d360adf58a2f024d3
winterrdog <winterrdog@protonmail.ch>
2023-08-28 22:22:02 +0300

    removed local-strategy authentication
    removed local strategy auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { LocalStrategy } from "./strategies/local.strategy";
 import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "../users/users.module";
 import { JwtModule } from "@nestjs/jwt";
@@ -21,7 +20,7 @@ dotenv.config();
             signOptions: { algorithm: "HS384", expiresIn: "3d" },
         }),
     ],
-    providers: [AuthService, LocalStrategy, JwtStrategy],
+    providers: [AuthService, JwtStrategy],
     exports: [AuthService],
 })
 export class AuthModule {}
