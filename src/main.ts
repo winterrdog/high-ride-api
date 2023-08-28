1f443d05fe3bce69567e1d722d210a918f9b2588
winterrdog <winterrdog@protonmail.ch>
2023-08-28 17:01:50 +0300

    added rate limiting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 // load environment variables
 import * as dotenv from "dotenv";
@@ -9,9 +10,19 @@ dotenv.config();
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    // todo: add app middleware
     app.enableCors();
+
+    // security headers
     app.use(helmet());
+
+    // rate limiting
+    const limiter = rateLimit({
+        windowMs: 20 * 60 * 1000, // 20 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+        message:
+            "Too many requests from this IP, please try again later in 20 minutes",
+    });
+    app.use(limiter);
 
     await app.listen(process.env.PORT ?? 3000);
 }
