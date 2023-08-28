dae731bb383a2011d757fc2db3f2a28cd6288783
winterrdog <winterrdog@protonmail.ch>
2023-08-28 12:57:00 +0300

    added ride management

--- src/rides/rides.controller.ts
+++ src/rides/rides.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Req,
+    UseGuards,
+} from "@nestjs/common";
+import { JwtAuthGuard } from "../auth/guards/jwt.auth.guard";
+import { RolesGuard } from "../auth/guards/roles.guard";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { RidesService } from "./rides.service";
+import { CreateRideDto, UpdateRideStatusDto } from "./dto/rides.dto";
+import { Request } from "express";
 
 @Controller("rides")
-export class RidesController {}
+@UseGuards(JwtAuthGuard) // allow authenticated users only
+export class RidesController {
+    constructor(private rideService: RidesService) {}
+
+    @UseGuards(RolesGuard)
+    @Roles("driver") // only drivers get rides
+    @Get()
+    async fetchAllRides(@Req() req: Request) {
+        // todo: check if driver's available
+        const rides = await this.rideService.queryAllRides(req);
+
+        if (rides instanceof Error) throw rides;
+
+        return rides;
+    }
+
+    @UseGuards(RolesGuard)
+    @Roles("passenger") // only passengers create rides
+    @Post()
+    async createRide(@Body() reqBody: CreateRideDto, @Req() req: Request) {
+        const ride = await this.rideService.createRide(req, reqBody);
+
+        if (ride instanceof Error) throw ride;
+
+        return ride;
+    }
+
+    @Patch(":id")
+    async updateRideStatus(
+        @Param("id") id: string,
+        @Body() reqBody: UpdateRideStatusDto,
+        @Req() req: Request,
+    ) {
+        const ride = await this.rideService.updateRideStatus(id, req, reqBody);
+
+        if (ride instanceof Error) throw ride;
+
+        return ride;
+    }
+}
--- src/rides/rides.service.ts
+++ src/rides/rides.service.ts
@@ -1,61 +1,134 @@
 import {
+    ForbiddenException,
     Injectable,
     InternalServerErrorException,
     NotFoundException,
 } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { Ride } from "./schemas/rides.schema";
+import { Ride, RideDocument } from "./schemas/rides.schema";
+import { CreateRideDto, UpdateRideStatusDto } from "./dto/rides.dto";
+import { Request } from "express";
+import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class RidesService {
-    constructor(@InjectModel(Ride.name) private rideModel: Model<Ride>) {}
+    constructor(
+        @InjectModel(Ride.name) private rideModel: Model<Ride>,
+        private userService: UsersService,
+    ) {}
 
-    // todo: add pagination
     // get all rides
-    async queryAllRides(): Promise<Ride[]> {
+    async queryAllRides(req: Request) {
         try {
+            // check if a driver's status is available
+            const user = await this.userService.queryUserFromRequest(req);
+
+            if (user instanceof Error) return user;
+
+            if (user.driverStatus !== "available") {
+                return new ForbiddenException(
+                    "You can't get rides if you're not available.",
+                );
+            }
+
             const rides = await this.rideModel.find();
             if (!rides) {
-                throw new NotFoundException("No rides found.");
+                return new NotFoundException("No rides found.");
             }
 
             return rides;
         } catch (_) {
-            throw new InternalServerErrorException(
+            return new InternalServerErrorException(
                 "Internal Server Error occurred",
             );
         }
     }
 
     // create a new ride
-    async createRide(ride: Ride): Promise<Ride> {
+    async createRide(req: Request, ride: CreateRideDto) {
         try {
-            const newRide = new this.rideModel(ride);
+            const user = await this.userService.queryUserFromRequest(req);
+
+            if (user instanceof Error) return user;
+
+            const newRide = new this.rideModel({
+                passenger: user._id,
+                pickUpLocation: ride.pickupLocation,
+                dropOffLocation: ride.dropoffLocation,
+            });
 
             return await newRide.save();
         } catch (_) {
-            throw new InternalServerErrorException(
+            return new InternalServerErrorException(
                 "Internal Server Error occurred",
             );
         }
     }
 
     // update a ride's status
-    async updateRideStatus(rideId: string, rideStatus: string): Promise<Ride> {
+    async updateRideStatus(
+        rideId: string,
+        req: Request,
+        reqBody: UpdateRideStatusDto,
+    ) {
         try {
-            const ride = await this.rideModel.findByIdAndUpdate(
-                rideId,
-                { rideStatus },
-                { new: true },
-            );
-            if (!ride) {
-                throw new NotFoundException("Ride not found.");
+            const user = await this.userService.queryUserFromRequest(req);
+
+            if (user instanceof Error) return user;
+
+            let ride;
+
+            // if user's a driver and ride status is accepted,
+            // update driver & ride status
+            if (user.role === "driver" && reqBody.rideStatus === "accepted") {
+                // check if driver's available
+                if (user.driverStatus !== "available") {
+                    return new ForbiddenException(
+                        "You can't accept a ride if you're not available.",
+                    );
+                }
+
+                // check if driver's already accepted a ride
+                const driver = await this.rideModel.findOne({
+                    driver: user._id,
+                });
+                if (driver) {
+                    return new ForbiddenException(
+                        "You cannot accept a new ride if you have already accepted another ride.",
+                    );
+                }
+
+                ride = await this.rideModel.findByIdAndUpdate(
+                    rideId,
+                    { driver: user._id, rideStatus: reqBody.rideStatus },
+                    { new: true },
+                );
+            } else {
+                // if user's passenger, they can't mark a ride as
+                // accepted or completed
+                if (
+                    user.role === "passenger" &&
+                    (reqBody.rideStatus === "accepted" ||
+                        reqBody.rideStatus === "completed")
+                ) {
+                    return new ForbiddenException(
+                        "Passengers can't mark rides as accepted or completed.",
+                    );
+                }
+
+                ride = await this.rideModel.findByIdAndUpdate(
+                    rideId,
+                    { rideStatus: reqBody.rideStatus },
+                    { new: true },
+                );
             }
+
+            if (!ride) return new NotFoundException("Ride not found.");
 
             return ride;
         } catch (_) {
-            throw new InternalServerErrorException(
+            return new InternalServerErrorException(
                 "Internal Server Error occurred",
             );
         }
