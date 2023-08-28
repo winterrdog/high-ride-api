83550ae42841720fe60b3dce0332ee14c12f4c37
winterrdog <winterrdog@protonmail.ch>
2023-08-28 12:55:36 +0300

    added authorization into users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,15 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./schemas/users.schema";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
+import { AuthService } from "../auth/auth.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ],
-    providers: [UsersService],
+    providers: [UsersService, AuthService, JwtService],
     controllers: [UsersController],
+    exports: [UsersService],
 })
 export class UsersModule {}
