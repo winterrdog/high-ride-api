1c035d9e8dbab454b07615b7113c8292358fbabc
winterrdog <winterrdog@protonmail.ch>
2023-08-28 12:53:04 +0300

    added users module to rides module

--- src/rides/rides.module.ts
+++ src/rides/rides.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { RidesService } from "./rides.service";
 import { Ride, RideSchema } from "./schemas/rides.schema";
 import { RidesController } from "./rides.controller";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: Ride.name, schema: RideSchema }]),
+        UsersModule,
     ],
     providers: [RidesService],
     controllers: [RidesController],
